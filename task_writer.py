b71dc348f043f777f5b79742a38b06e9d9d90742
calum hand <crh53@bath.ac.uk>
2023-01-04 19:55:02 +0000

    updating simulation input

--- task_writer.py
+++ task_writer.py
@@ -8,41 +8,47 @@ from tqdm import tqdm
 
 def xe_kr_input(mof_name, cuttoff):
     sim_details = F"""SimulationType                MonteCarlo
-NumberOfInitializationCycles  1000
 NumberOfCycles                1000
-Forcefield                    raspa
-ChargeMethod                  None
-CutOffVDW                     {cuttoff}
+NumberOfInitializationCycles  1000
+PrintEvery                    250
+Restart File                  no
+ChargeMethod                  none
+CutOff                        {cuttoff}
 
-Framework 0 
-            FrameworkName                 {mof_name}
-            RemoveAtomNumberCodeFromLabel no
-            Movies                        no
-            UnitCells                     2 2 2
-            ExternalTemperature           273
-            ExternalPressure              10e5
+Framework 0
+FrameworkName {mof_name}
+UnitCells 2 2 2
+ExternalTemperature 273
+ExternalPressure 1e6
+RemoveAtomNumberCodeFromLabel yes
 
-Component 0 MoleculeName                  xenon
-            MoleculeDefinition            local
-            MolFraction                   0.20
-            FugacityCoefficient           0.9253
-            ReinsertionProbability        1.0
-            SwapProbability               1.0
-            TranslationProbability        1.0
-            IdentityChangeProbability     1.0
-                NumberOfIdentityChanges   2
-                IdentityChangesList       0 1
+Component 0 MoleculeName        xenon
+ChargeMethod                    None
+IdealGasRosenbluthWeight        1.0
+FugacityCoefficient             0.9253
+MoleculeDefinition              local
+MolFraction                     0.20
+IdentityChangeProbability       1.0
+  NumberOfIdentityChanges       2
+  IdentityChangesList           0 1
+TranslationProbability          1.0
+ReinsertionProbability          1.0
+SwapProbability                 1.0
+CreateNumberOfMolecules         0
 
-Component 1 MoleculeName                  krypton
-            MoleculeDefinition            local
-            MolFraction                   0.80
-            FugacityCoefficient           0.9671
-            ReinsertionProbability        1.0
-            SwapProbability               1.0
-            TranslationProbability        1.0
-            IdentityChangeProbability     1.0
-                NumberOfIdentityChanges   2
-                IdentityChangesList       0 1
+Component 1 MoleculeName        krypton
+ChargeMethod                    None
+IdealGasRosenbluthWeight        1.0
+FugacityCoefficient             0.9671
+MoleculeDefinition              local
+MolFraction                     0.80
+IdentityChangeProbability       1.0
+  NumberOfIdentityChanges       2
+  IdentityChangesList           0 1
+TranslationProbability          1.0
+ReinsertionProbability          1.0
+SwapProbability                 1.0
+CreateNumberOfMolecules         0
     """
     return sim_details
 
@@ -60,12 +66,13 @@ if not os.path.exists(FF_OUT):
 # PARAMETERS TO EVALUATE
 forcefield_path = F'forcefields/{FF}'
 cifs = [(c.replace('.cif', ''), os.path.join('cifs', c)) for c in os.listdir('cifs') if '.cif' in c]
-cuttoff_radii = [12, 14, 16]
+cuttoff_radii = [12.0, 14.0, 16.0]
 n_repeats = 5
 
 
 # WRITING TASKS
 tasks = []
+count = 1
 for mof, mof_path in tqdm(cifs):
     for r in cuttoff_radii:
         for n_ in range(n_repeats):
@@ -77,11 +84,10 @@ for mof, mof_path in tqdm(cifs):
             with open(os.path.join(dir_name, 'simulation.input'), 'w') as f:
                 f.write(xe_kr_input(mof, r))
             
-            tasks.append(F'tsp hare run --rm -it -v $PWD/{dir_name}:/app {IMAGE} simulate simulation.input')
-        
-
-# dont need the output of these so just delete them (needs to be done through the docker image because of permission)
-tasks.append(F'tsp hare run --rm -it -v $PWD/{dir_name}:/app {IMAGE} rm -rf */Movies/ */VTK/ */Restart')
+            tasks.append(F'echo "starting simulation [{count}/165] at $(date)"')
+            tasks.append(F'hare run --rm -it -v $PWD/{dir_name}:/app {IMAGE} simulate simulation.input')
+            tasks.append(F'echo "simulation completed at $(date)"')
+            count += 1
         
         
 with open('tasks.sh', 'w') as f:
