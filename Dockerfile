5cf6edb47882ed15ec187c00c69c45da2a334387
Bundaberg-Joey <calumhand94@hotmail.com>
2023-03-01 17:06:04 +0000

    updating files

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN apt-get -y update \
     && apt-get -y install
 
 RUN conda install -c conda-forge raspa2
+RUN python3 -m pip install pandas numpy ase tqdm --no-cache-dir
 
 WORKDIR /app
--- task_writer.py
+++ task_writer.py
@@ -4,7 +4,55 @@ from uuid import uuid4
 from tqdm import tqdm
 import pandas as pd
 
-from simulations.input import xe_kr_input
+# -------------------------------------------------------------------------------------
+
+
+def xe_kr_input(mof_name):
+    sim_details = F"""SimulationType                MonteCarlo
+NumberOfCycles                1000
+NumberOfInitializationCycles  1000
+PrintEvery                    250
+Restart File                  no
+ChargeMethod                  none
+CutOff                        16.0
+
+Framework 0
+FrameworkName {mof_name}
+UnitCells 2 2 2
+ExternalTemperature 273
+ExternalPressure 1e6
+RemoveAtomNumberCodeFromLabel yes
+
+Component 0 MoleculeName        xenon
+ChargeMethod                    None
+IdealGasRosenbluthWeight        1.0
+FugacityCoefficient             0.9253
+MoleculeDefinition              local
+MolFraction                     0.20
+IdentityChangeProbability       1.0
+  NumberOfIdentityChanges       2
+  IdentityChangesList           0 1
+TranslationProbability          1.0
+ReinsertionProbability          1.0
+SwapProbability                 1.0
+CreateNumberOfMolecules         0
+
+Component 1 MoleculeName        krypton
+ChargeMethod                    None
+IdealGasRosenbluthWeight        1.0
+FugacityCoefficient             0.9671
+MoleculeDefinition              local
+MolFraction                     0.80
+IdentityChangeProbability       1.0
+  NumberOfIdentityChanges       2
+  IdentityChangesList           0 1
+TranslationProbability          1.0
+ReinsertionProbability          1.0
+SwapProbability                 1.0
+CreateNumberOfMolecules         0
+    """
+    return sim_details
+
 
 # -------------------------------------------------------------------------------------
 # GLOBALS
